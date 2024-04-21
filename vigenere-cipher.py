b81f82f6aa43f4cfc06de2ac1b1afbb938925dbd
Brela1shala <brela.shala@student.uni-pr.edu>
2024-04-21 14:25:35 +0200

    fixed a bug, added print

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -1,13 +1,14 @@
 # 1. Hapi pare - gjenerojme celesin nga keystream duke perdorur nje int32 ose string si seed.
-    import random
-    
+
     def generate_keystream(seed, length):
     if isinstance(seed, int):
         # Të zbatohet kufiri 32-bit në seed
         seed = seed % (2**32)
+        import random
         random.seed(seed)
         keystream = [random.randint(0, 255) for _ in range(length)]
         elif isinstance(seed, str):
+        
         keystream = [ord(char) for char in seed]
           while len(keystream) < length:
             keystream.extend([ord(char) for char in seed])
@@ -50,6 +51,10 @@ keystream = generate_keystream(seed, len(plaintext))
 # 7. Deklarojm variablat encrypted text dhe decrypted text, per ti pasur gati per printim.
 encrypted_text = vigenere_encrypt(plaintext.encode(), keystream)
 decrypted_text = vigenere_decrypt(encrypted_text, keystream)
+
+print("Plaintext:", plaintext)
+print("Encrypted text:", encrypted_text)
+print("Decrypted text:", decrypted_text.decode())
 
 
 
